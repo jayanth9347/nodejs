fed8a12f5d9b67ffd63ce755aae4bd5637152c0c
jayanth <pujalajayanth9347@gmail.com>
2024-07-18 09:17:55 +0000

    commit message

---
+++ user-auth-app/db.js
@@ -0,0 +1,18 @@
+// db.js
+
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/userAuthApp', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    console.log('MongoDB connected')
+  } catch (error) {
+    console.error(error.message)
+    process.exit(1)
+  }
+}
+
+module.exports = connectDB
